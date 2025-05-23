1d6d2b5761e3a1cf4d8f2c794475522cb3b0cd66
mdeekshith00 <mdeekshith000@gmail.com>
2025-05-23 20:06:21 +0530

    added Hollow Triangle Pattern

--- src/Patterns/AllPatterns.java
+++ src/Patterns/AllPatterns.java
@@ -37,6 +37,9 @@ public class AllPatterns {
 		Butterfly(n);
 		System.out.println("----------Rhombus-----------");
 		rhombus(n);
+		System.out.println("----------hollowTriangle-----------");
+		hollowTriangle(n);
+
 	}
 	
 	// printing rectangle pattern 
@@ -246,7 +249,17 @@ public class AllPatterns {
 //		Hollow Triangle Pattern
 		public static void hollowTriangle(int n) {
 			for(int i=1; i<=n; i++) {
-				
+				for(int j=i; j<n; j++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=(2 *i -1); j++) {
+					if(j ==1  || i ==n || j == (2 * i -1)) {
+						System.out.print("*");
+					}else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
 			}
 		}
 
